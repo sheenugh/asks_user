d8609a7f8c4b60829c557bac50b387067836c06a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-03-23 19:13:54 +0800

    Adding Pseudo code and instruction.txt

---
+++ asks_user.py
@@ -0,0 +1,14 @@
+# Delima, Sheena Mae D.
+# BSCPE 1-2
+# March 23, 2024
+
+# -----------------------------------------------------------------------------
+
+# ========= PSEUDO CODE =========
+# || LIBRARIES/PACKAGES ||
+
+
+# || ACTUAL CODES || 
+# - HTML template in asking the user
+# - Code for collecting the data using POST and GET HTTP Method
+# - Code for running the local host
